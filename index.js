41f657dd374ce7469a5ceba8a91de8aa1048c08d
huangjitao <164173925@qq.com>
2023-04-24 16:36:47 +0800

    chore: 部署配置项

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+ * @Title: 页面点击跳转事件
+ * @Author: huangjitao
+ * @Date: 2023-04-24 16:02:46
+ * @Description: import.meta.env.BASE_URL 只能在module中使用
+ */
+
+const btns = document.querySelectorAll(".open-page")
+
+const baseUrl = import.meta.env.BASE_URL
+
+btns.forEach((i) => {
+  const href = i.getAttribute("href")
+  i.onclick = window.open(`${baseUrl}${href}`)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 /*
- * @Title: your project
+ * @Title: 配置文件
  * @Author: huangjitao
  * @Date: 2023-04-24 15:36:37
  * @Description: description of this file
  */
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/'
+  base: process.env.NODE_ENV === 'development' ? '/' : '/<REPO>/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        "1-introduction": resolve(__dirname, '1-introduction/index.html'),
+        "2-basicOption/objectOption.html": resolve(__dirname, '2-basicOption/objectOption.html'),
+        "2-basicOption/animation.html": resolve(__dirname, '2-basicOption/animation.html'),
+        "2-basicOption/guiTools": resolve(__dirname, '2-basicOption/guiTools.html'),
+      },
+    },
+  },
 })
