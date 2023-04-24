c23e02a54f98ff6a8da5bc78f8df714209b0f407
huangjitao <164173925@qq.com>
2023-04-24 15:37:26 +0800

    chore: vite配置

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+/*
+ * @Title: your project
+ * @Author: huangjitao
+ * @Date: 2023-04-24 15:36:37
+ * @Description: description of this file
+ */
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/<REPO>/'
+})
