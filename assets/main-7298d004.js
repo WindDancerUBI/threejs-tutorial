6087a3578042451d6a3605730d22e51394b3154a
WindDancerUBI <WindDancerUBI@users.noreply.github.com>
2023-04-24 08:49:49 +0000

    deploy: f3d2996b019a40e0d6cfbe4fe1dd098505b4d2f6

---
+++ assets/main-7298d004.js
@@ -0,0 +1,1 @@
+import"./reset-33ef1466.js";const o=document.querySelectorAll(".open-page"),n="/threejs-tutorial/";o.forEach(e=>{const t=e.getAttribute("href");e.onclick=window.open(`${n}${t}`)});
--- assets/main-7ecce24e.js
+++
@@ -1,1 +0,0 @@
-import"./reset-33ef1466.js";const t=document.querySelectorAll(".open-page"),n="/%3CREPO%3E/";t.forEach(e=>{const o=e.getAttribute("href");e.onclick=window.open(`${n}${o}`)});
