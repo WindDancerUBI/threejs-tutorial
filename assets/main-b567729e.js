1cd5467cd1c78a70ab79536d108bffe8673efe55
WindDancerUBI <WindDancerUBI@users.noreply.github.com>
2023-04-25 07:40:49 +0000

    deploy: b7e972902b3a8a7c5e6bc004f0065a533aef4bbe

---
+++ assets/main-b567729e.js
@@ -0,0 +1,1 @@
+import"./reset-56fd7512.js";const o=document.querySelectorAll(".open-page"),n="/threejs-tutorial/";o.forEach(e=>{const t=e.getAttribute("href");e.onclick=()=>window.open(`${n}${t}`)});
